55c474e35ea616d4c9b8d3392f8b8dfbe783fc64
Vladimir Smetana <v.smetana@yadro.com>
2025-09-25 23:38:26 +0300

    stable equations without good coefs

--- ballistics.py
+++ ballistics.py
@@ -9,9 +9,6 @@ import constants
 
 parser = rp.rocket_parser(path.rocket_lib + "master_rocket.json")
 
-k1a = 4.1
-k2a = 2
-
 attack_list = []
 time_list = []
 
@@ -22,9 +19,6 @@ class ballistics:
         self.vel = vel
         self.alt = alt
         
-        time_sep = parser.get_work_time()
-        self.alpha = att.alpha(k1a, k2a, time_sep[0], False)
-
         self.G = aero.UnionStream()
         self.G.set_elnumber(parser.get_block_number())
         self.G.set_diameter(parser.get_diameters())
@@ -40,7 +34,7 @@ class ballistics:
     def update_params(self, time):
         self.thrust =  parser.get_thrust_from_time(time)
         self.mass   =  parser.get_mass_from_time(time)
-        self.attack = self.alpha.calculate_alpha(self.vel, time)*math.pi/180
+        self.attack = parser.get_attack(self.vel, time)*math.pi/180
         self.G.calculate_CXY(self.vel, self.alt, self.attack)
 
         self.atm = atmo.atmosphere(self.alt)
@@ -54,19 +48,17 @@ class ballistics:
         attack_list.append(self.attack*180/math.pi)
         time_list.append(time)
 
-        time += parser.interstep
-
     def delta_velocity(self, time):
         self.update_params(time)
         F_P = self.thrust * math.cos(self.attack)
-        F_X = self.G.CX*57.3*self.dencity*parser.maximum_area*self.vel**2/2
-        return F_P/self.mass - F_X/self.mass - self.atm.get_AOG() * math.sin(self.Y)
+        F_X = self.G.CX*self.dencity*parser.maximum_area*self.vel**2/2
+        return (F_P - F_X)/self.mass - self.atm.get_AOG() * math.sin(self.Y)
 
     def delta_trajangle(self, time):
         self.update_params(time)
         F_P = self.thrust * math.sin(self.attack)
-        F_Y = self.G.CY*self.attack*self.dencity*parser.maximum_area*self.vel**2/2
-        F_G = ((self.atm.get_AOG() * math.cos(self.Y)) * (1-self.vel**2/2)/(self.atm.get_AOG()*(constants.earth_radius + self.alt)))
+        F_Y = self.G.CY*self.dencity*parser.maximum_area*self.vel**2/2
+        F_G = self.atm.get_AOG() * math.cos(self.Y) * (1 - self.vel**2 / (self.atm.get_AOG() * (constants.earth_radius + self.alt))) 
         return (F_P + F_Y)/(self.mass*self.vel) - F_G/self.vel
     
     def delta_polar(self, time):
@@ -86,13 +78,14 @@ from scipy.integrate import solve_ivp
 v_max = 0
 
 def system(t, vars):
-    n, y, v, h = vars
+    n, y, v, h, l = vars
     b = ballistics(n, y, v, h)
     dn = b.delta_polar(t)
     dy = b.delta_trajangle(t)
     dv = b.delta_velocity(t)
     dh = b.delta_altitude(t)
-    return [dn, dy, dv, dh]
+    dl = b.delta_longitude(t)
+    return [dn, dy, dv, dh, dl]
 
 def event_stop_velocity(t, vars):
     v = vars[2]
@@ -108,7 +101,7 @@ if __name__ == "__main__":
     ft = parser.get_full_time()
     h = parser.interstep
     t_span = (0, ft-1)
-    y0 = [0, math.pi/2, 0.1, 0.1]
+    y0 = [0, math.pi/2, 0.1, 0.1, 0.1]
     
     sol = solve_ivp(system, t_span, y0, method='RK45', max_step=h, events=event_stop_velocity)
     print("Max velocity: ", v_max)
@@ -132,10 +125,10 @@ if __name__ == "__main__":
     plt.grid(True)
 
     plt.subplot(4,1,3)
-    plt.plot(sol.t, sol.y[3]/1000, label='Высота alt(t)', color='green')
-    plt.xlabel('Время, с')
+    plt.plot(sol.y[4]/1000, sol.y[3]/1000, label='Высота alt(t)', color='green')
+    plt.xlabel('Дальность, км')
     plt.ylabel('Высота, км')
-    plt.title('Высота по времени')
+    plt.title('Траектория')
     plt.legend()
     plt.grid(True)
 
